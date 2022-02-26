24e5452bc7fd67e1485103cee27011a7b093f18d
sajolbhadra <sajolbhadra@gmail.com>
2022-02-27 00:59:45 +0600

    typescript first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var abc = 'hellow world!';
+console.log(abc);
+ 
---
+++ index.ts
@@ -0,0 +1,10 @@
+const abc:string = 'hellow world!'
+// console.log(abc)
+
+function getLowerCase(str:string){
+    return str.toLowerCase();
+}
+
+function add (num1:number, num2:number){
+    return num1 + num2;
+}
